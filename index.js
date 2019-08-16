e4625ab3a7a58d115d7de2d408653eb93c330164
Didre <ceci21062@gmail.com>
2019-08-15 22:52:20 -0500

    funcion de comparacion la extencion del archivo y pasa test ya con la funcionalidad estructurada

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-module.exports = () => {
-  return true;
+const path = require("path")
+module.exports = filePath => path.extname(filePath) === ".md"     ;  
+
+/*module.exports = (filePath) => {
+    if (path.extname(filePath)=== ".md"){
+        return true;
+    }
+    return false;
 };
+ternarios
+   path.extname(filePath)=== ".md") ? true : false
+
+module.exports = (filePath) => path.extname(filePath)=== ".md") ? true : false   o
+
+module.exports = filePath => path.extname(filePath) === ".md" ? true : false      o
+
+*/
+
+              
+
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,17 +1,13 @@
-const mdLinks = require('../');
 
+const mdLinks = require('../index.js');
 
 describe('mdLinks', () => {
 
   it('should return true for a valid .md file', () => {
-    expect(mdLiks("../README.md"))toBe(true)
+    expect(mdLinks("../README.md")).toBe(true)
   });
 
-  it('should return true for a valid .md file', () => {
-    expect(mdLiks("../index.js"))toBe(false)
+  it('should return false for a valid .md file', () => {
+    expect(mdLinks("../index.js")).toBe(false)
   });
-}
-  
-
-
 });
